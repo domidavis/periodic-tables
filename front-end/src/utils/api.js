a09c8d4de69e7b5b146fd4a133aadf51c088e4d9
Burnout Queen <burnout.qu33n@gmail.com>
2022-04-28 15:22:09 -0400

    oiwehg

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@
 import formatReservationDate from "./format-reservation-date";
  import formatReservationTime from "./format-reservation-date";
  
- const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://domi-tables-backend.herokuapp.com";
+ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
  
  const headers = new Headers();
  headers.append("Content-Type", "application/json");
