edee7c8e0286e738cdac6a75946c10853de093b8
Burnout Queen <burnout.qu33n@gmail.com>
2022-04-28 11:43:02 -0400

    Add 'heroku-frontend/' from commit '05d3159522b13e4f5d62f0cda9f65b1b57968099'
    git-subtree-dir: heroku-frontend
    git-subtree-mainline: 05d3159522b13e4f5d62f0cda9f65b1b57968099
    git-subtree-split: 05d3159522b13e4f5d62f0cda9f65b1b57968099

---
+++ heroku-frontend/front-end/src/dashboard/Dashboard.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { listReservations, listTables } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
+import useQuery from "../utils/useQuery";
+import { previous, today, next } from "../utils/date-time";
+import { useHistory } from "react-router";
+import ReservationTable from "../layout/reservations/ReservationTable";
+import DisplayTables from "../layout/tables/DisplayTables";
+import { FontAwesome } from "react-icons/fa";
+
+/**
+ * Defines the dashboard page.
+ * @param date
+ *  the date for which the user wants to view reservations.
+ * @returns {JSX.Element}
+ */
+function Dashboard({ date }) {
+  const history = useHistory();
+  const query = useQuery();
+  const searchDate = query.get("date");
+  date = searchDate ? searchDate : date;
+  const [reservations, setReservations] = useState([]);
+  const [reservationsError, setReservationsError] = useState(null);
+
+  const [tables, setTables] = useState([]);
+  const [tablesError, setTablesError] = useState(null);
+
+  useEffect(loadDashboard, [date]);
+
+  function loadDashboard() {
+    const abortController = new AbortController();
+    setReservationsError(null);
+    listReservations({ date }, abortController.signal)
+      .then(setReservations)
+      .catch(error => setReservationsError(error));
+    return () => abortController.abort();
+  }
+
+  useEffect(() => {
+    setTablesError(null);
+
+    async function loadTables() {
+      try {
+        const teebles = await listTables();
+        setTables(teebles);
+      }
+      catch(e) {
+        setTablesError(e);
+      }
+    }
+    loadTables();
+  }, []);
+
+  const handlePrevious = (event) => {
+    history.push(`dashboard?date=${previous(date)}`);
+  }
+  const handleToday = (event) => {
+    history.push(`dashboard?date=${today(date)}`);
+  }
+  const handleNext = (event) => {
+    history.push(`dashboard?date=${next(date)}`);
+  }
+
+  console.log(reservations);
+  console.log(tables);
+  return (
+    <main className="m-3">
+      <h1>Dashboard</h1>
+      <div>
+        <ErrorAlert error={reservationsError} />
+        <div>
+          <div id="button-div">
+            <button className="btn btn-info m-2" onClick={handlePrevious}>&#8249;</button>
+            <button className="btn btn-info m-2" onClick={handleToday}>Today</button>
+            <button className="btn btn-info m-2" onClick={handleNext}>&#8250;</button>
+          </div>
+          <h3 className="mt-5">Reservations for {date}</h3>
+            <ReservationTable reservations={reservations} setError={setReservationsError}/>
+          <div className="mt-3">
+            <ErrorAlert error={tablesError} />
+            <DisplayTables tables={tables} setError={setTablesError}/>
+          </div>
+          </div>
+        </div>
+    </main>
+  );
+}
+
+export default Dashboard;
