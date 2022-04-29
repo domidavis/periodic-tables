b9d728d967f7b43ef51355212642833f7ce35aa7
Burnout Queen <burnout.qu33n@gmail.com>
2022-04-28 20:09:19 -0400

    moved navbar to top

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -13,16 +13,24 @@ import "./Layout.css";
  */
 function Layout() {
   return (
-    <div className="container-fluid">
-      <div className="row h-100">
-        <div className="col-md-2 side-bar">
+    <div>
+      <div className="row-md-2" style={{ backgroundColor: "#1f424b" }}>
+        <Menu />
+      </div>
+
+      <div className="container">
+        <div className="row-12 h-100">
+        {/* <div className="col-md-2 side-bar">
           <Menu />
-        </div>
-        <div className="col">
-          <Routes />
+        </div> */}
+          <div className="col">
+            <Routes />
+          </div>
         </div>
       </div>
     </div>
+    
+    
   );
 }
 
--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -10,28 +10,25 @@ import { Link } from "react-router-dom";
 
 function Menu() {
   return (
-    <nav className="navbar navbar-dark align-items-start p-2">
-      <div className="container-fluid d-flex flex-column p-0">
-        <Link
-          className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
+    <nav className="navbar fixed-top navbar-expand-md navbar-dark domi-the-boss-bar ">
+      <Link
+          className="navbar-brand"
           to="/"
         >
           <div className="sidebar-brand-text mx-3">
             <span>Periodic Tables</span>
           </div>
         </Link>
-        <hr className="sidebar-divider my-0" />
-        <ul className="nav navbar-nav text-light" id="accordionSidebar">
-          <li className="nav-item">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse justify-content-end" id="navbarsExample04">
+        <ul className="navbar-nav  ">
+          <li className="nav-item active">
             <Link className="nav-link" to="/dashboard">
               <span className="oi oi-dashboard" />
               &nbsp;Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/search">
-              <span className="oi oi-magnifying-glass" />
-              &nbsp;Search
             </Link>
           </li>
           <li className="nav-item">
@@ -46,14 +43,13 @@ function Menu() {
               &nbsp;New Table
             </Link>
           </li>
+          <li className="nav-item ">
+            <Link className="nav-link" to="/search">
+              <span className="oi oi-magnifying-glass" />
+              &nbsp;Search
+            </Link>
+          </li>          
         </ul>
-        <div className="text-center d-none d-md-inline">
-          <button
-            className="btn rounded-circle border-0"
-            id="sidebarToggle"
-            type="button"
-          />
-        </div>
       </div>
     </nav>
   );
